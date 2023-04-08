410522899318e6bb734bb5dd7faf11463a9c9dfe
M-dora <sunkumasthanbabu@gmail.com>
2023-04-08 10:21:50 +0530

    DONE

---
+++ JAVA SCRIPT/Array/three.js
@@ -0,0 +1,9 @@
+let = size ['1','2','3']
+console.logo(a)
+var x;
+let y;
+
+
+x = 5;
+y = 6;
+let z = x + y;
--- React-1/react app/React-Jan-23-main/Backup/class1to6/src/Navbar/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-let Navbar = () => {
-
-    return <nav className="navbar navbar-dark bg-dark">
-        <a href="#" className="navbar-brand">React bootstrap</a>
-    </nav>
-}
-export default Navbar
